773d69f19bbc2e12804f7c341b088c453d9b7313
blackcoder0x01 <66354644+blackcoder0x01@users.noreply.github.com>
2020-10-03 01:57:34 +0600

    Update and rename portchecker.sh to otx.sh

---
+++ otx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+input=$1
+while IFS= read -r line;do
+  echo -e "[+] Enumeration ---------> $line"
+  echo  ""
+  gron "https://otx.alienvault.com/otxapi/indicator/hostname/url_list/$line?limit=100&page=1" | grep "\burl\b" | gron --ungron | jq
+  echo -e ""
+  echo -e ""
+done < "$input" >> $2
